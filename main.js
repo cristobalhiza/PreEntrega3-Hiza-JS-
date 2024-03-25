48bf91efce1d3a1066c8b5e1696c01e97f94046f
cristobalhiza <cristobalhiza@gmail.com>
2024-03-24 22:44:27 -0300

    codigo javascript actividad x dia

--- main.js
+++ main.js
@@ -7,6 +7,13 @@ const rafting = 30;
 
 let total = 0;
 
+// let actividadesSeleccionadas = [];
+
+// function agregarActividad(actividad) {
+//   actividadesSeleccionadas.push(actividad);
+//   mostrarPrecioTotal();
+// }
+
 const precioBtn = document.querySelector("#precioTotalBtn");
 // Función para calcular el precio total de la actividad
 function calcularPrecioTotal(actividad, numDias) {
@@ -36,21 +43,22 @@ function calcularPrecioTotal(actividad, numDias) {
 
 // Función para mostrar el precio total de la actividad seleccionada
 function mostrarPrecioTotal() {
-    const actividadSeleccionada = document.querySelector("#selector-reserva").value;
-    const numDias = parseInt(document.getElementById("numDias").value);
+  const actividadSeleccionada =
+    document.querySelector("#selector-reserva").value;
+  const numDias = parseInt(document.getElementById("numDias").value);
+  const precioTotal = calcularPrecioTotal(actividadSeleccionada, numDias);
+  document.getElementById("precioTotal").innerHTML = precioTotal;
+
+  if (!isNaN(numDias) && numDias > 0) {
     const precioTotal = calcularPrecioTotal(actividadSeleccionada, numDias);
     document.getElementById("precioTotal").innerHTML = precioTotal;
+  } else {
+    document.getElementById("precioTotal").innerHTML = "";
+    alert("Por favor, ingresa un número válido de días.");
+  }
+}
 
-    if (!isNaN(numDias) && numDias > 0) {
-        const precioTotal = calcularPrecioTotal(actividadSeleccionada, numDias);
-        document.getElementById("precioTotal").innerHTML = precioTotal;
-      } else {
-        document.getElementById("precioTotal").innerHTML = "";
-        alert("Por favor, ingresa un número válido de días.");
-      }
-    }
-
-  // Añadimos un evento al selector de actividad para mostrar el precio total
+// Añadimos un evento al selector de actividad para mostrar el precio total
 precioBtn.addEventListener("click", mostrarPrecioTotal);
 
 // Mostramos el precio total por defecto
