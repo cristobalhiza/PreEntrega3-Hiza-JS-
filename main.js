a91da8b0e6599d2584988708ea8a9f0bdac6f350
cristobalhiza <cristobalhiza@gmail.com>
2024-03-26 14:56:55 -0300

    termina formulario reserva y agrega page info

--- main.js
+++ main.js
@@ -12,19 +12,28 @@ let actividadesSeleccionadas = [];
 function actualizarSeleccion() {
   actividadesSeleccionadas = [];
   document.querySelectorAll('input[type="checkbox"]:checked').forEach((checkbox) => {
+    const cantidad = parseInt(document.getElementById(checkbox.value + "Cantidad").value);
+    if (isNaN(cantidad) || cantidad <= 0) {
+      alert("Por favor, ingresa una cantidad válida para la actividad " + checkbox.value + ".");
+    } else {
     actividadesSeleccionadas.push({
       actividad: checkbox.value,
-      cantidad: parseInt(document.getElementById(checkbox.value + "Cantidad").value)
+      cantidad: cantidad
     });
+  }
   });
 }
+
 // Añadir un evento de escucha para el cambio en los checkboxes
 document.querySelectorAll('input[type="checkbox"]').forEach((checkbox) => {
   checkbox.addEventListener("change", function() {
     const campoCantidad = document.getElementById(checkbox.value + "Cantidad");
+    const textoCantidad = document.getElementById(checkbox.value + "Texto");
     if (checkbox.checked) {
+      textoCantidad.classList.remove('d-none');
       campoCantidad.classList.remove('d-none'); // Mostrar el campo de cantidad si el checkbox está marcado
     } else {
+      textoCantidad.classList.add('d-none');
       campoCantidad.classList.add('d-none'); // Ocultar el campo de cantidad si el checkbox no está marcado
     }
     actualizarSeleccion();
@@ -50,28 +59,58 @@ function agregarActividad(actividad) {
 } 
 
 // Función para calcular el precio total de la actividad
-function calcularPrecioTotal(numDias) {
+function calcularPrecioTotal() {
   let precioTotal = 0;
   for (const actividadSeleccionada of actividadesSeleccionadas) {
     precioTotal += preciosActividades[actividadSeleccionada.actividad] * actividadSeleccionada.cantidad;
   }
-  return precioTotal * numDias;
+  return precioTotal;
 }
-document.querySelector("#numDias").addEventListener("change", mostrarPrecioTotal);
 
 // Función para mostrar el precio total de la actividad seleccionada
 function mostrarPrecioTotal() {
-  const numDias = parseInt(document.getElementById("numDias").value);
-  const precioTotal = calcularPrecioTotal(numDias);
+  const precioTotal = calcularPrecioTotal();
   const precioTotalElement = document.getElementById("precioTotal");
 
-  if (!isNaN(numDias) && numDias > 0) {
+  if (precioTotal > 0) {
     precioTotalElement.textContent = precioTotal;
   } else {
-    precioTotalElement.textContent = "";
-    alert("Por favor, ingresa un número válido de días.");
+    precioTotalElement.textContent = "0";
   }
 }
 
-// Mostramos el precio total por defecto
-mostrarPrecioTotal();
+// FORMULARIO RESERVA
+
+function guardarReserva(evento) {
+  evento.preventDefault();
+
+  // Obetener información de contacto
+  const nombre = document.querySelector('#nombreInput').value;
+  const correo = document.querySelector('#correoInput').value;
+  const fechaLlegada = document.querySelector('#fechaInput').value;
+
+  // Obtener precio total
+  const precioTotal = document.getElementById("precioTotal").textContent;
+
+  // Crear objeto reserva
+
+  const reserva = {
+    nombre: nombre,
+    correo: correo,
+    fechaLlegada: fechaLlegada,
+    precioTotal: precioTotal,
+  };
+
+  // Convertir objeto reserva a string 
+  const reservaJSON = JSON.stringify(reserva);
+
+  // Guardar reserva en localStorage
+  localStorage.setItem('reserva', reservaJSON);
+
+  alert('Reserva guardada exitosamente!');
+  window.location.assign("/index.html")
+}
+
+document.querySelector('#formReserva').addEventListener("submit", guardarReserva);
+
+
